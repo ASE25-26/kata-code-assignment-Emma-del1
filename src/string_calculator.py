7df111dacdcd64ef83ed9948372c7ab016238b9d
st193650 <st193650@stud.uni-stuttgart.de>
2025-10-23 00:20:51 +0200

    Patch for the problem

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,27 +1,26 @@
 import re
 
-from astroid.brain.brain_numpy_utils import numpy_supports_type_hints
-
 
 def add(numbers: str) -> int:
     # Implementiere die String-Kalkulator-Funktion hier
     if not numbers:
         return 0
-    standardDelimiter = ",|\n"
+    standard_delimiter = ",| \n"
 
     if numbers.startswith("//"):
-        parts = numbers.split("\n",1)
-        delimiterPart = parts[0][2:]
+        parts = numbers.split("\n", 1)
+        delimiter_part = parts[0][2:]
         numbers = parts[1]
 
-        if delimiterPart.startswith("[") and delimiterPart.endswith("]"):
-            standardDelimiter = "|".join(re.escape(d) for d in re.findall(r"\[(.*?)\]", delimiterPart))
+        if delimiter_part.startswith("[") and delimiter_part.endswith("]"):
+            standard_delimiter = "|".join(re.escape(d) for d in re.findall(r"\[(.*?)\]",
+                                                                          delimiter_part))
         else:
-            standardDelimiter = re.escape(delimiterPart)
+            standard_delimiter = re.escape(delimiter_part)
 
-    nums = [int(n) for n in re.split(standardDelimiter, numbers)]
+    nums = [int(n) for n in re.split(standard_delimiter, numbers)]
 
     neg = [n for n in nums if n < 0]
     if neg:
-        raise ValueError(f"Negatives not allowed: {neg}")
+        raise ValueError("Negatives not allowed: {neg}")
     return sum(n for n in nums if n <= 1000)
